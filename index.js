ab7cb5a72f2c6a843a68c67a6318940a97395a5b
ASHRAFUR RAHMAN <you@example.com>
2022-10-14 10:06:49 +0600

    updated

--- index.js
+++ index.js
@@ -9,8 +9,11 @@ const app = express();
 app.use(cors())
 app.use(express.json());
 
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.7rbiakh.mongodb.net/?retryWrites=true&w=majority`;
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.d9sn0vy.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+
 
 async function run(){
 
